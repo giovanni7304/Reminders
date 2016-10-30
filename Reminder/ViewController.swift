6880dc873fa9bf164d092d74fb66c9b27c984169
Terry Johnson <giovanni7304@me.com>
2016-10-29 23:23:22 -0500

    Initial Commit

--- Reminder/ViewController.swift
+++ Reminder/ViewController.swift
@@ -8,20 +8,68 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var importantCheckBox: NSButton!
+    @IBOutlet weak var tableView: NSTableView!
+    
+    var reminderItems : [Reminder] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        getReminderItems()
     }
-
+    
+    func getReminderItems() {
+        
+        if let context = (NSApplication.shared().delegate as? AppDelegate)?.managedObjectContext {
+            
+            do {
+                reminderItems = try context.fetch(Reminder.fetchRequest())
+                print(reminderItems.count)
+            } catch {}
+        }
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
 
-
+    @IBAction func addClicked(_ sender: AnyObject) {
+        if textField.stringValue != "" {
+            if let context = (NSApplication.shared().delegate as? AppDelegate)?.managedObjectContext {
+                let reminder = Reminder(context: context)
+                reminder.name = textField.stringValue
+                if importantCheckBox.state == 0 {
+                    reminder.important = false
+                } else {
+                    reminder.important = true
+                }
+                (NSApplication.shared().delegate as? AppDelegate)?.saveAction(nil)
+                textField.stringValue = ""
+                importantCheckBox.state = 0
+                
+                getReminderItems()
+            }
+        }
+    }
+    
+    //MARK: TableView Stuff
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return reminderItems.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.make(withIdentifier: "importantCell", owner: self) as? NSTableCellView {
+            cell.textField?.stringValue = "YELLOW"
+            return cell
+        }
+        return nil
+    }
+    
 }
 
