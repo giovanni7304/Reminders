af1420860e647bd2d2dd85427650639f47994a5b
Terry Johnson <giovanni7304@me.com>
2016-10-29 22:13:47 -0500

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Terry Johnson on 10/29/16.
+//  Copyright © 2016 Terry Johnson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
